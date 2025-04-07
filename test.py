5823f58b3af5f1c9016236a20d13e7f87b0295a7
Lucas Lo Giudice <lucas.logiudice@outlook.fr>
2025-04-07 13:31:15 +0200

    test

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import torch
+from ddpm import unet
 
 print("-----LAUNCH script test-----")
 
 print("torch.version", torch.__version__)
+print("unet", unet)
 print("torch.cuda.is_available()", torch.cuda.is_available())
 print("torch.cuda.device_count()", torch.cuda.device_count())
 print("torch.cuda.current_device()", torch.cuda.current_device())
 print("torch.cuda.device(torch.cuda.current_device())", torch.cuda.device(torch.cuda.current_device()))
 
 print("-----CLOSE script test-----")
-
-from ddpm import unet
