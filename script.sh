a5bf08d05a735250f7665024cc0b46044e16033a
Lucas Lo Giudice <lucas.logiudice@outlook.fr>
2025-04-16 16:15:15 +0200

    test

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
-echo "Ceci est un test"
+echo 'Removing previous container if it exists'
+docker rm -f medicaldiffusion
+echo "Image building ..."
 docker build -t medicaldiffusion:train .
+echo "Container launching ..."
 docker run --gpus '"device=0"' --name medicaldiffusion --shm-size=16gb medicaldiffusion:train     
