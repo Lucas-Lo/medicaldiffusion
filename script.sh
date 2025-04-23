4cb1b1d651429303867cdf36fc422a6db157b2e3
Lucas Lo Giudice <lucas.logiudice@outlook.fr>
2025-04-23 17:39:56 +0200

    try to save data

--- script.sh
+++ script.sh
@@ -3,5 +3,15 @@ echo 'Removing previous container if it exists'
 docker rm -f medicaldiffusion
 echo "Image building ..."
 docker build -t medicaldiffusion:train .
+echo "Log folder creation"
+rm -f -R log_medicaldiffusion
+mkdir log_medicaldiffusion
 echo "Container launching ..."
-docker run --gpus '"device=0"' --name medicaldiffusion --shm-size=16gb medicaldiffusion:train     
+# docker run --gpus '"device=0"' --name medicaldiffusion --shm-size=16gb medicaldiffusion:train
+docker run --gpus '"device=0"' --name medicaldiffusion --shm-size=16gb -v $(pwd)/log_medicaldiffusion:/medicaldiffusion medicaldiffusion:train
+echo "Waiting for container to finish..."
+docker container wait medicaldiffusion
+echo "try to copy container"
+rm -f -R container_copy
+mkdir container_copy
+docker container cp -r medicaldiffusion:/ $(pwd)/container_copy
